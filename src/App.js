7b483974b7829d4f859aba2e26cbf58e46bc66ac
Ga Beo <ancs21.ps@gmail.com>
2018-05-25 11:25:18 +0700

    origin version

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>It's Ok 👌!!</h1>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+
+};
+
+export default App;
